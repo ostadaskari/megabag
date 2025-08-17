f5dff93bc15c26e49494e72b547e92bf69755149
Your Name <you@example.com>
2025-08-17 15:30:59 +0330

    edit style img cover in gallery images

--- design/views/manager/products_list_view.php
+++ design/views/manager/products_list_view.php
@@ -100,7 +100,7 @@
   <div class="row">
     <div class="col-12 col-md-6">
      <div class="container">
-        <div class="row">
+        <div class="row mt-4">
         <div class="col-12 my-2">
             <strong>Name:</strong> Example Part
             </div>
@@ -126,20 +126,11 @@
             <strong>Tag:</strong> New
             </div>
             <div class="col-12 my-2 d-flex flex-row">
-                <div class="d-flex align-items-center">
-                    <svg width="18" height="18" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"></path>
-                    </svg>
-                    <label >Datasheet:</label>
-                </div> 
-                <ul class="mt-1 list-group small  d-flex justify-content-between align-items-center" style="max-height: 180px;overflow-y:auto;">
-                    <li class="d-flex flex-row align-items-center justify-content-between itemfile border shadow-sm p-2">
-                        <a style="color:#101010;" href="../../uploads/pdfs/6893379f2eb20_Test-pdf_4.pdf" target="_blank">
-                            <span>Test-pdf_5.pdf</span>  
-                        </a>
-                    </li>
-                </ul>  
-        </div> 
+                <strong>Datasheet:</strong>
+                <a style="color:rgb(8, 55, 126);" href="../../uploads/pdfs/6893379f2eb20_Test-pdf_4.pdf" target="_blank">
+                    <span>Test-pdf_5.pdf</span>  
+                </a>
+            </div> 
         </div>
     </div>
     </div> 
@@ -148,22 +139,22 @@
     <div>
         <label class="form-label">Images:</label>
         <div class="imgCover mb-2">
-        <img src="../../design/assets/img/img14.jpg" class="img-fluid w-100">
-    </div>   
+          <img src="../../design/assets/img/img14.jpg" class="img-fluid w-100">
+        </div>   
     <ul class="mt-1 list-group small d-flex justify-content-between align-items-center" style="max-height: 180px; overflow-y:auto;">
-        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm p-2">
+        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm">
             <span><img class="img-fluid" src="../../design/assets/img/img11.jpg"></span>                             
         </li>
-        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm p-2">
+        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm">
             <span><img class="img-fluid" src="../../design/assets/img/img12.jpg"></span>                             
         </li>
-        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm p-2">
+        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm">
             <span><img class="img-fluid" src="../../design/assets/img/img13.jpg"></span>                             
         </li>
-        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm p-2">
+        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm">
             <span><img class="img-fluid" src="../../design/assets/img/img14.jpg"></span>                             
         </li>
-        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm p-2">
+        <li class="d-flex flex-row align-items-center justify-content-between itemfile itemImg border shadow-sm">
             <span><img class="img-fluid" src="../../design/assets/img/img11.jpg"></span>                             
         </li>
     </ul>
