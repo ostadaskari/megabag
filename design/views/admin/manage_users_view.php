54db7253193834042ecd2b2a93c4317fa10641c3
Your Name <you@example.com>
2025-08-17 14:58:11 +0330

    edit style modalDetails

--- design/views/admin/manage_users_view.php
+++ design/views/admin/manage_users_view.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
   <div class="d-flex flex-row align-items-center justify-content-between titleTop">
-=======
-<div class="d-flex flex-row align-items-center justify-content-between mb-3 titleTop">
->>>>>>> edc74decd4c521aa0a79eff6e0017d67c28c48fc
         <h2 class="d-flex align-items-center">
-        <svg width="26" height="26" fill="currentColor" class="bi bi-people me-2 mx-1" viewBox="0 0 16 16">
+        <svg width="26" height="26" fill="#fff" class="bi bi-people me-2 mx-1" viewBox="0 0 16 16">
         <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
         </svg>  
         Manage Users</h2>
@@ -16,16 +13,11 @@
         </a>
     </div>
 <div class="container px-0">
-<<<<<<< HEAD
     <input type="text" id="searchInput" class="form-control mb-2 border shadow-sm" placeholder="Search by name, family, or nickname">
     <div class="table-responsive fixed-table-container border rounded shadow-sm bg-light p-1" id="userTableContainer" style="max-height: 73vh;">
         <!-- AJAX content will be loaded here -->
     </div>
-=======
-    <input type="text" id="searchInput" class="form-control mb-3 border shadow-sm" placeholder="Search by name, family, or nickname" autocomplete="off"> 
-    <div class="table-responsive fixed-table-container border rounded shadow-sm bg-light p-1" id="userTableContainer">
-        </div>
->>>>>>> edc74decd4c521aa0a79eff6e0017d67c28c48fc
+
 </div>
 
 
