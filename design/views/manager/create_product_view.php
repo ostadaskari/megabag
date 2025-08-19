bc25eddebd9a09465a7e5f0248d3ebe9f3eb097e
Your Name <you@example.com>
2025-08-19 15:50:37 +0330

    edit style in "edit product"

--- design/views/manager/create_product_view.php
+++ design/views/manager/create_product_view.php
@@ -18,7 +18,7 @@
             <form  method="post" enctype="multipart/form-data" class="d-flex flex-column partForm">
                <!-- part number and Manufacturer inputs -->
                 
-                <div class="container bg-light border rounded shadow-sm p-3 mb-2">
+                <div class="container bg-light border rounded shadow-sm p-2 mb-2">
                 <div class="d-flex flex-row align-items-center mb-2">
                   <svg width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
@@ -88,7 +88,7 @@
 
 
                 <!-- inputs files -->
-                <div class="d-flex flex-row align-items-center my-3">
+                <div class="d-flex flex-row align-items-center my-2">
                     <svg width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
@@ -136,11 +136,12 @@
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="" style="text-align: end">
+                <div class="" style="text-align: end">
                 <button type="submit" class="btn btn-primary" id="Addpart">Add part</button>
             </div>
+            </div>
+
+            
         </div>
     </form>
 </div>
