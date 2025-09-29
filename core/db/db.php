338ba79d728c436f2b71821b6622f2e8c944196b
ostadaskari <askaraskarinejad@outlook.com>
2025-09-29 01:08:27 -0700

    ignore test

--- core/db/db.php
+++ core/db/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";   // in host : dohteksc_megabag_u
 $pass = "";  // in host : !G0baU%vZMak;rFr
-$dbname = "mega_db";  //  in host : dohteksc_megabag
+$dbname = "mega_db";  //  in host : dohteksc_megabag lllllllllllllllllllllllll
 
 // Create database connection
 $conn = new mysqli($host, $user, $pass, $dbname);
